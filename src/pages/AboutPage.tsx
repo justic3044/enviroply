5c50d3bfc0dae031c709c0750a31c83f29ec505e
justic3044 <justic3.044@gmail.com>
2025-10-08 09:35:33 +0200

    Update AboutPage.tsx

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -92,7 +92,7 @@ const AboutPage = () => {
       {/*Industries */}
       <section className="py-20 bg-gray-50">
         <div className="container mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="grid grid-cols-1 lg:grid-cols-2 gap-12">
+          <div className="grid grid-cols-1 lg:grid-cols-1 gap-1">
             {/* Industries Served */}
             <div className="bg-white rounded-xl shadow-lg p-8">
               <h2 className="text-2xl font-bold text-gray-900 mb-6">Industries We Serve</h2>
